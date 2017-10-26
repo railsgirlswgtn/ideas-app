49ecebe36774aa545b7fc096022570b89fdd6d1f
Van Veñegas <van.venegas@powershop.co.nz>
2017-10-27 12:23:43 +1300

    Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/update'
-
   root to: "ideas#index"
 
   resources :ideas
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
