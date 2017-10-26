2fe87a322dfc6fe03b8764c13577f65633081b01
Van Veñegas <van.venegas@powershop.co.nz>
2017-10-27 12:29:43 +1300

    Adding signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(params.require(:user).permit(:username, :password))
+
+    if @user.save
+      redirect_to root_path, notice: 'User was successfully created.'
+    else
+      render :new
+    end
   end
 
   def update
